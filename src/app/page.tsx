85661d60f2fa1a4c197666adcf0bb1fa0051ba9d
MD Waliullah <waliullah9099@gmail.com>
2024-05-03 23:54:26 +0600

    first commit

---
+++ src/app/page.tsx
@@ -0,0 +1,12 @@
+import Footer from "@/components/shared/footer/Footer";
+import Navbar from "@/components/shared/navbar/Navbar";
+
+export default function Home() {
+  return (
+    <>
+      <Navbar />
+      <div className="h-screen"></div>
+      <Footer />
+    </>
+  );
+}
